dca757be4993bd10ac33f81d4278f9fba684e9fb
PriyaRNair27 <priyarnair2710@gmail.com>
2022-02-18 11:33:33 +0530

    friendsapp

---
+++ app/src/main/java/com/example/friendsapp/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.friendsapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1,e2,e3,e4;
+    AppCompatButton b1;
+    String  getname,getfndname,getnickname,getdescribe;
+    String apiUrl=" https://dummyapifriends.herokuapp.com/adddata";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=(EditText) findViewById(R.id.name);
+        e2=(EditText) findViewById(R.id.fndname);
+        e3=(EditText) findViewById(R.id.nickname);
+        e4=(EditText) findViewById(R.id.describe);
+        b1=(AppCompatButton) findViewById(R.id.submit);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getname=e1.getText().toString();
+                getfndname=e2.getText().toString();
+                getnickname=e3.getText().toString();
+                getdescribe=e4.getText().toString();
+                StringRequest sr=new StringRequest(Request.Method.POST,
+                        apiUrl,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                e1.setText("");
+                                e2.setText("");
+                                e3.setText("");
+                                e4.setText("");
+                                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                            HashMap<String,String>params=new HashMap<>();
+                            params.put("name",getname);
+                            params.put("friendName",getfndname);
+                            params.put("friendNickName",getnickname);
+                            params.put("DescribeYourFriend",getdescribe);
+                        return params;
+
+                    }
+                };
+                RequestQueue rq= Volley.newRequestQueue(getApplicationContext());
+                rq.add(sr);
+
+
+
+            }
+        });
+    }
+}
